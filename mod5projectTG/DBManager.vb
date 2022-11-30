53eaa3f0a62d751e22b235e77c34739b83b4500c
theagregory <thea.isis@gmail.com>
2022-11-30 15:50:18 -0500

    Create displaylist,addnew

--- mod5projectTG/DBManager.vb
+++ mod5projectTG/DBManager.vb
@@ -32,29 +32,46 @@ Public Class DBManager
 
                 .DataSource = datatable
 
-                .Columns(0).HeaderText = "ID"
+                .Columns(0).HeaderText = "Employee Number"
                 .Columns(1).HeaderText = "Firstname"
                 .Columns(2).HeaderText = "Lastname"
-                .Columns(3).HeaderText = "Age"
-                .Columns(4).HeaderText = "Gender"
-                .Columns(5).HeaderText = "Status"
-                .Columns(6).HeaderText = "ID"
-                .Columns(7).HeaderText = "Firstname"
-                .Columns(8).HeaderText = "Lastname"
-                .Columns(9).HeaderText = "Age"
-                .Columns(10).HeaderText = "Gender"
-                .Columns(11).HeaderText = "Status"
-                .Columns(12).HeaderText = "ID"
-                .Columns(13).HeaderText = "Firstname"
-                .Columns(14).HeaderText = "Lastname"
+                .Columns(3).HeaderText = "DOB"
+                .Columns(4).HeaderText = "Address"
+                .Columns(5).HeaderText = "City"
+                .Columns(6).HeaderText = "Prov."
+                .Columns(7).HeaderText = "Postal Code"
+                .Columns(8).HeaderText = "Home"
+                .Columns(9).HeaderText = "Cell"
+                .Columns(10).HeaderText = "Start Date"
+                .Columns(11).HeaderText = "SIN"
+                .Columns(12).HeaderText = "Hourly"
+                .Columns(13).HeaderText = "Education"
+                .Columns(14).HeaderText = "Access Level"
+                .Columns(15).HeaderText = "Status"
 
-
-
+                .Columns(0).Width = 100
+                .Columns(1).Width = 100
+                .Columns(2).Width = 100
+                .Columns(3).Width = 100
+                .Columns(4).Width = 300
+                .Columns(5).Width = 100
+                .Columns(6).Width = 100
+                .Columns(7).Width = 50
+                .Columns(8).Width = 100
+                .Columns(9).Width = 100
+                .Columns(10).Width = 100
+                .Columns(11).Width = 75
+                .Columns(12).Width = 50
+                .Columns(13).Width = 50
+                .Columns(14).Width = 50
+                .Columns(15).Width = 50
 
             End With
 
-        Catch ex As Exception
+            Me.connect.Close()
 
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Connection Failed")
         End Try
 
     End Sub
